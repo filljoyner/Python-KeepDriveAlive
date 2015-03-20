bc9fe6d25de7ce90d00621d4a11246575d65729a
Philip Joyner <filljoyner@gmail.com>
2015-03-20 16:18:22 -0500

    won't crash if drive isn't available, remove unused library

--- kda.py
+++ kda.py
@@ -1,4 +1,3 @@
-import os
 from time import sleep, gmtime, strftime
 
 drives = ['D', 'E']
@@ -8,15 +7,19 @@ def writeFile(_drives):
 	filename = '___kda___.txt'
 	strToWrite = strftime('%Y-%m-%d %H:%M:%S', gmtime())
 
+	print('---')
+
 	for d in drives:
 		filepath = d + ':/' + filename
 
-		_file = open(filepath, 'w+')
-		_file.write(strToWrite)
-		_file.close()
+		try:
+			_file = open(filepath, 'w+')
+			_file.write(strToWrite)
+			_file.close()
 
-		print(strToWrite)
-
+			print(d + ' | ' + strToWrite)
+		except:
+			print(d + ' | ' + strToWrite + ' UNAVAILABLE')
 
 
 if __name__=="__main__":
